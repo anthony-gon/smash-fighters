f6ab81ebe923e08498708547d22c4f4ea00b2056
Anthony Gonzalez <anthonygonzalez@Anthonys-Air-8.quinnipiac.edu>
2024-10-08 17:51:02 -0400

    Blank bugs

--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -18,15 +18,60 @@ public class TestMap extends Map {
 
     public TestMap() {
         super("test_map.txt", new CommonTileset());
-        this.playerStartPosition = getMapTile(2, 11).getLocation();
+        this.playerStartPosition = getMapTile(5, 116).getLocation();
     }
 
     @Override
     public ArrayList<Enemy> loadEnemies() {
         ArrayList<Enemy> enemies = new ArrayList<>();
 
-        BugEnemy bugEnemy = new BugEnemy(getMapTile(16, 10).getLocation().subtractY(25), Direction.LEFT);
+        BugEnemy bugEnemy = new BugEnemy(getMapTile(1, 10).getLocation(), Direction.LEFT);
         enemies.add(bugEnemy);
+
+        BugEnemy bugEnemy1 = new BugEnemy(getMapTile(2, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy1);
+
+        BugEnemy bugEnemy2 = new BugEnemy(getMapTile(3, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy2);
+
+        BugEnemy bugEnemy3 = new BugEnemy(getMapTile(4, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy3);
+
+        BugEnemy bugEnemy4 = new BugEnemy(getMapTile(5, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy4);
+
+        BugEnemy bugEnemy5 = new BugEnemy(getMapTile(6, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy5);
+
+        BugEnemy bugEnemy6 = new BugEnemy(getMapTile(7, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy6);
+
+        BugEnemy bugEnemy7 = new BugEnemy(getMapTile(8, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy7);
+
+        BugEnemy bugEnemy8 = new BugEnemy(getMapTile(9, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy8);
+
+        BugEnemy bugEnemy9 = new BugEnemy(getMapTile(10, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy9);
+
+        BugEnemy bugEnemy10 = new BugEnemy(getMapTile(11, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy10);
+
+        BugEnemy bugEnemy11 = new BugEnemy(getMapTile(12, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy11);
+
+        BugEnemy bugEnemy12 = new BugEnemy(getMapTile(13, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy12);
+
+        BugEnemy bugEnemy13 = new BugEnemy(getMapTile(14, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy13);
+
+        BugEnemy bugEnemy14 = new BugEnemy(getMapTile(15, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy14);
+
+        BugEnemy bugEnemy15 = new BugEnemy(getMapTile(0, 10).getLocation(), Direction.LEFT);
+        enemies.add(bugEnemy15);
 
         DinosaurEnemy dinosaurEnemy = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
         enemies.add(dinosaurEnemy);
