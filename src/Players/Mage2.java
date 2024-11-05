90caf7c4950f1c43abd122097e4439ae4b9556ec
zippyh <zippyh@icloud.com>
2024-11-05 18:01:54 -0500

    Added attack for gunner

--- src/Players/Mage2.java
+++ src/Players/Mage2.java
@@ -76,7 +76,7 @@ public class Mage2 extends Player {
 
                         // add fireball enemy to the map for it to spawn in the level
                         map.addEnemy(fireball);
-                        shootTimer = 50;
+                        shootTimer = 51;
                 }
                 if (shootTimer > 0) {
                         shootTimer--;
